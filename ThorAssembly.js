84a7e5792324d639e706c12863efa261bc381c3a
angellm <alm37454@gmail.com>
2018-07-25 22:44:19 +0200

    Will it work?

--- ThorAssembly.js
+++ ThorAssembly.js
@@ -10,16 +10,30 @@ var m_data      = require("data");
 var m_preloader = require("preloader");
 var m_ver       = require("version");
 var m_nla       = require("nla");
+var scene 		= b4w.require("scenes");
 
-var importantFrames = [75, 125, 175, 275, 350, 425, 575];
+var importantFrames = [75, 125, 175, 275, 350, 400, 500];
 var animationIndex = 0;
 
-/*var materials = [
-    ["1x Art1Body piece", "3x M3 Nut", "3x M3x20mm Screw"],
-    ["2x Art1Body piece", "2x M3 Nut", "2x M3x20mm Screw"]
+var materials = [
+	["GripperBodyBot", "6x M3 Nut"],
+	["Servomotor", "4x M3x12mm Screw"],
+	["Servomotor disk", "GripperActiveArm", "3x Servomotor Screw"],
+	["GripperPassiveArm", "GripperBodyTop", "1x M3x12mm Screw", "2x M3x20mm Screw"],
+	["2x GripperArm", "2x M3 Nut", "2x M3x20mm Screw"],
+	["GripperFingerRight", "GripperFingerLeft", "4x M3 Lock Nut", "4x M3x16mm Screw"]
 ];
 
-*/
+var showObjects = [
+	['BG00', 'BG01', 'BG01.001', 'BG02', 'BG04', 'BG05', 'BG06', 'BG07', 'BG25', 'BG26'],
+	['BG08', 'BG09', 'BG10', 'BG11', 'BG12'],
+	['BG13', 'BG14', 'BG15', 'BG16', 'BG17'],
+	['BG18', 'BG19', 'BG20', 'BG21', 'BG22'],
+	['BG23', 'BG24', 'BG27','BG28'],
+	['BG29', 'BG30', 'BG31', 'BG32', 'BG33', 'BG34', 'BG35', 'BG36', 'BG38']
+]
+
+
 // detect application mode
 var DEBUG = (m_ver.type() == "DEBUG");
 
@@ -96,20 +110,19 @@ function load_cb(data_id, success) {
     }
 
     m_app.enable_camera_controls();
-
-    // place your code here
-    // m_nla.set_frame(40);
-
+	hideAllObjects();
     m_nla.stop();
-
-
 }
 
 function button_previous_click(e) {
-    if (animationIndex>0 && !m_nla.is_play()){
-        m_nla.set_frame(importantFrames[animationIndex-1]);
-        animationIndex--;
-        document.getElementById("stepCounter").innerHTML = animationIndex.toString().concat("/100");
+    if (animationIndex>1 && !m_nla.is_play()){
+        m_nla.set_frame(importantFrames[animationIndex-2]);
+		m_nla.set_range(importantFrames[animationIndex-2], importantFrames[animationIndex-1]);
+        listItems(animationIndex-2);
+		hideShowObjects(animationIndex-1)
+		animationIndex--;
+        document.getElementById("stepCounter").innerHTML = animationIndex.toString().concat("/6");
+		m_nla.play();
     }
 }
 
@@ -117,7 +130,6 @@ function button_replay_click(e) {
     if (animationIndex>0 && !m_nla.is_play()){
         m_nla.set_frame(importantFrames[animationIndex-1]);
         m_nla.set_range(importantFrames[animationIndex-1], importantFrames[animationIndex]);
-        deleteListItems();
         m_nla.play();
     }
 }
@@ -127,22 +139,21 @@ function button_next_click(e) {
         m_nla.set_frame(importantFrames[animationIndex]);
         m_nla.set_range(importantFrames[animationIndex], importantFrames[animationIndex+1]);
         listItems(animationIndex);
+		hideShowObjects(animationIndex)
         animationIndex++;
-        document.getElementById("stepCounter").innerHTML = animationIndex.toString().concat("/100");
+        document.getElementById("stepCounter").innerHTML = animationIndex.toString().concat("/6");
         m_nla.play();
     }
 }
 
 function listItems(i){
-    /*deleteListItems();
+    deleteListItems();
     var list = document.getElementById('list');
-    var j;
-    for (j=0; j<materials[i].length; j++){
+    for (var j=0; j<materials[i].length; j++){
         var entry = document.createElement('li');
         entry.appendChild(document.createTextNode(materials[i][j]));
         list.appendChild(entry);
-    }*/
-	return;
+    }
 }
 
 function deleteListItems(){
@@ -151,6 +162,30 @@ function deleteListItems(){
         list.removeChild(list.firstChild);
     }
 }
+
+function hideAllObjects(){
+	var scene_object_list = scene.get_all_objects("MESH");
+	for (var j=0; j<scene_object_list.length; j++){
+		scene.hide_object(scene_object_list[j]);
+	}
+}
+
+function hideShowObjects(animationIndex){
+	for (var j=0; j<showObjects[animationIndex].length; j++){
+		hideShow(showObjects[animationIndex][j]);
+	}
+}
+
+function hideShow(objectName){
+	var objectHideShow = scene.get_object_by_name(objectName);
+	if (scene.is_hidden(objectHideShow)){
+		scene.show_object(objectHideShow);
+	}
+	else{
+		scene.hide_object(objectHideShow);
+	}
+}
+
 
 });
 
